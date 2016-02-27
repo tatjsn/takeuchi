47aabd1bd360350558f331121f705d58915dfe1b
Taz Jackson <tatjsn@users.noreply.github.com>
2016-02-27 22:54:27 +0900

    Enable displaying without id

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Top from './Top';
 import Info from './Info';
-import { connect } from 'react-redux';
 
-const App = ({ cardId, birthDate, top, info }) => (
+export default ({ cardId, birthDate, top, info }) => (
   <div>
     <p>
       {cardId}({birthDate}){(new Date()).toLocaleTimeString()}
     </p>
     <Top {...top} />
-    <Info {...info} />
+    { (cardId && birthDate) ? <Info {...info} /> : '番号と誕生日を入力すると待ち順番がみれます。' }
   </div>
-);
-
-export default connect(({ top, info }) => ({ top, info }))(App);
+)
--- src/client/entry.js
+++ src/client/entry.js
@@ -3,22 +3,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import queryString from 'query-string';
 import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import App from './components/App';
 import app from './reducers';
 import thunkMiddleware from 'redux-thunk';
+import { connect } from 'react-redux';
 import { fetchTop, fetchInfo } from './actions';
 
-const query = queryString.parse(window.location.search);
+const { id, bd } = queryString.parse(window.location.search);
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware // lets us dispatch in actions
 )(createStore);
 const store = createStoreWithMiddleware(app);
 store.dispatch(fetchTop());
-store.dispatch(fetchInfo(query.id, query.bd));
+if (id && bd) {
+  store.dispatch(fetchInfo(id, bd));
+}
+
+const AppC = connect(({ top, info }) => ({ top, info }))(App);
 
 ReactDOM.render((
   <Provider store={store}>
-    <App cardId={query.id} birthDate={query.bd} />
+    <AppC cardId={id} birthDate={bd} />
   </Provider>
 ), document.getElementById('app'));
